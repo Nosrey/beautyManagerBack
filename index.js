77a7554086caba8eb919944d34a6bcede88fef74
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-12-30 04:28:02 -0400

    ahora si carpeta api

--- index.js
+++ index.js
@@ -17,8 +17,8 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js').default;
-const { conn } = require('./src/db.js');
+const server = require('./api/app.js');
+const { conn } = require('./api/db.js');
 const port = 3001
 
 // Syncing all the models at once.
